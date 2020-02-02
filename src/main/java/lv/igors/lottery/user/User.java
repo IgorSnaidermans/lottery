6e1b5e3251027792a10e3f6cea38803f0415a043
igorssnaidermans <igors.snaidermans@icloud.com>
2020-02-02 02:14:00 +0200

    validation

--- src/main/java/lv/igors/lottery/user/User.java
+++ src/main/java/lv/igors/lottery/user/User.java
@@ -2,14 +2,21 @@ package lv.igors.lottery.user;
 
 import lombok.*;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+
 @Builder
 @Getter
 @Setter
 @EqualsAndHashCode
 @ToString
 public class User {
-    //todo email validation
+    @Email
+    @NotBlank
     String email;
-    //todo from 21 age
+
+    @Min(21)
+    @NotBlank
     int age;
 }
