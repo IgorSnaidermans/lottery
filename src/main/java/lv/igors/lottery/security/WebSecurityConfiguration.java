df4ea4cacfe31c52f66be47206e2cc6e8998c64a
igorssnaidermans <igors.snaidermans@icloud.com>
2020-02-14 02:07:02 +0200

    security applied to rest & web controllers

--- src/main/java/lv/igors/lottery/security/WebSecurityConfiguration.java
+++ src/main/java/lv/igors/lottery/security/WebSecurityConfiguration.java
@@ -2,33 +2,69 @@ package lv.igors.lottery.security;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 
 @Configuration
 @RequiredArgsConstructor
 @EnableWebSecurity
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
-    private final LotteryAuthenticationProvider lotteryAuthenticationProvider;
 
-    protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .and()
-                .csrf().disable()
-                .authenticationProvider(lotteryAuthenticationProvider)
-                .authorizeRequests()
-                .antMatchers("/", "/register", "/status", "/stats", "/lottery/**"
-                        , "/rest/register", "/rest/status", "/rest/stats", "/rest/lottery/**").permitAll()
-                .and()
-                .authorizeRequests()
-                .antMatchers("/admin/**", "/rest/admin/**").hasAuthority("ADMIN")
-                .and()
-                .formLogin()
-                .loginPage("/admin-login").permitAll()
-                .defaultSuccessUrl("/admin")
-                .failureUrl("/admin-login?error=true")
-                .and()
-                .logout().permitAll();
+
+    @Configuration
+    @Order(1)
+    @RequiredArgsConstructor
+    public static class ApiWebSecurityConfigurationAdapter extends WebSecurityConfigurerAdapter {
+        private final LotteryAuthenticationProvider lotteryAuthenticationProvider;
+
+        protected void configure(HttpSecurity http) throws Exception {
+            http.sessionManagement()
+                    .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+            http
+                    .antMatcher("/rest/admin/**")
+                    .csrf().disable()
+                    .authenticationProvider(lotteryAuthenticationProvider)
+                    .authorizeRequests()
+                    .antMatchers("/rest/admin/**").hasAuthority("ADMIN")
+                    .antMatchers("/rest/register", "/rest/status",
+                            "/rest/stats", "/rest/lottery/**").permitAll()
+                    .and()
+                    .httpBasic();
+        }
     }
+
+
+    @Configuration
+    @Order(2)
+    @RequiredArgsConstructor
+    public static class FormLoginWebSecurityConfigurerAdapter extends WebSecurityConfigurerAdapter {
+
+        private final LotteryAuthenticationProvider lotteryAuthenticationProvider;
+
+        protected void configure(HttpSecurity http) throws Exception {
+            http.authorizeRequests()
+                    .and()
+                    .csrf().disable()
+                    .authenticationProvider(lotteryAuthenticationProvider)
+                    //.antMatcher("/admin/**")
+                    .authorizeRequests()
+                    .antMatchers("/", "/register", "/status", "/stats", "/lottery/**").permitAll()
+                    .and()
+                    .authorizeRequests()
+                    .antMatchers("/admin/**").hasAuthority("ADMIN")
+                    .and()
+                    .formLogin()
+                    .loginPage("/admin-login").permitAll()
+                    .defaultSuccessUrl("/admin")
+                    .failureUrl("/admin-login?error=true")
+                    .and()
+                    .logout().permitAll();
+        }
+    }
+
 }
