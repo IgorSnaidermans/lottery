b2152577cbbf2770a8ac1b48363295f4c4aebb41
igorssnaidermans <igors.snaidermans@icloud.com>
2020-03-28 18:03:27 +0200

    refactor: extracted low level logic

--- src/main/java/lv/igors/lottery/code/CodeService.java
+++ src/main/java/lv/igors/lottery/code/CodeService.java
@@ -4,118 +4,95 @@ import lombok.RequiredArgsConstructor;
 import lv.igors.lottery.code.dto.CodeDTO;
 import lv.igors.lottery.statusResponse.Responses;
 import lv.igors.lottery.statusResponse.StatusResponse;
+import lv.igors.lottery.statusResponse.StatusResponseManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class CodeService {
     private static final Logger LOGGER = LoggerFactory.getLogger(CodeService.class);
-    private final CodeDAO codeDAO;
+    private final CodeEntityManager codeEntityManager;
+    private final StatusResponseManager statusResponseManager;
 
     public StatusResponse addCode(Code code) {
 
         if (!findSimilarCodes(code)) {
             LOGGER.info("Code saved " + code);
-
-            codeDAO.save(code);
-            return StatusResponse.builder()
-                    .status(Responses.OK.getResponse())
-                    .build();
+            codeEntityManager.save(code);
+            return statusResponseManager.buildOk();
         }
         LOGGER.warn("Unsuccessful code save due to already exist" + code);
-        return StatusResponse.builder()
-                .status(Responses.FAIL.getResponse())
-                .reason(Responses.CODE_EXIST.getResponse())
-                .build();
+        return statusResponseManager.buildFailWithMessage(Responses.CODE_EXIST.getResponse());
     }
 
-    public boolean findSimilarCodes(Code code) {
-        Optional<Code> possibleCode = codeDAO.findCodeByParticipatingCodeAndLotteryId(code.getParticipatingCode(),
-                code.getLotteryId());
-        return possibleCode.map(value -> value.equals(code)).orElse(false);
+    private boolean findSimilarCodes(Code code) {
+        try {
+            codeEntityManager.findCodeByParticipatingCodeAndLotteryId(code.getParticipatingCode(), code.getLotteryId());
+            return true;
+        } catch (CodeDoesntExistException e){
+            return false;
+        }
+
     }
 
     public StatusResponse checkWinnerCode(CodeDTO codeDTO, String lotteryWinningCode) {
         LOGGER.info("Checking winning status for " + codeDTO);
 
         Code winnerCode;
-        Code requestedCode = Code.builder()
-                .ownerEmail(codeDTO.getEmail())
-                .participatingCode(codeDTO.getCode())
-                .lotteryId(codeDTO.getLotteryId())
-                .build();
+        Code requestedCode = buildCode(codeDTO);
 
         try {
             if (!checkCodeOwner(requestedCode)) {
                 LOGGER.warn("Foreign code was requested by " + codeDTO.toString());
-                return StatusResponse.builder()
-                        .status(Responses.FAIL.getResponse())
-                        .reason(Responses.CODE_FOREIGN_CODE.getResponse())
-                        .build();
+                return statusResponseManager.buildFailWithMessage(Responses.CODE_FOREIGN_CODE.getResponse());
             }
-            winnerCode = getCodeByParticipatingCodeAndLotteryId(lotteryWinningCode,
+            winnerCode = codeEntityManager.getCodeByParticipatingCodeAndLotteryId(lotteryWinningCode,
                     requestedCode.getLotteryId());
         } catch (CodeDoesntExistException e) {
-            return StatusResponse.builder()
-                    .status(Responses.FAIL.getResponse())
-                    .reason(e.getMessage())
-                    .build();
+            return statusResponseManager.buildFailWithMessage(e.getMessage());
         }
 
-        if (winnerCode.equals(requestedCode)) {
-            return StatusResponse.builder()
-                    .status(Responses.CODE_WIN.getResponse())
-                    .build();
-        } else {
-            return StatusResponse.builder()
-                    .status(Responses.CODE_LOSE.getResponse())
-                    .build();
-        }
+        return checkWin(winnerCode, requestedCode);
+    }
 
+    private Code buildCode(CodeDTO codeDTO) {
+        return Code.builder()
+                    .ownerEmail(codeDTO.getEmail())
+                    .participatingCode(codeDTO.getCode())
+                    .lotteryId(codeDTO.getLotteryId())
+                    .build();
     }
 
     private boolean checkCodeOwner(Code code) throws CodeDoesntExistException {
         LOGGER.info("Checking code owner for " + code);
-        String requestedCodeOwnerEmail = getCodeByParticipatingCodeAndLotteryId(code.getParticipatingCode(),
+        String requestedCodeOwnerEmail = codeEntityManager.getCodeByParticipatingCodeAndLotteryId(code.getParticipatingCode(),
                 code.getLotteryId())
                 .getOwnerEmail();
         return requestedCodeOwnerEmail.equals(code.getOwnerEmail());
     }
 
-    public Code getCodeByParticipatingCodeAndLotteryId(String code, Long id) throws CodeDoesntExistException {
-        LOGGER.info("Getting code information for code:" + code);
-        Optional<Code> possibleCode = codeDAO.findCodeByParticipatingCodeAndLotteryId(code, id);
-
-        if (possibleCode.isPresent()) {
-            return possibleCode.get();
+    private StatusResponse checkWin(Code winnerCode, Code requestedCode) {
+        if (winnerCode.equals(requestedCode)) {
+            return statusResponseManager.buildWithMessage(Responses.CODE_WIN.getResponse());
         } else {
-            LOGGER.warn("Could not find code information for code:" + code);
-            throw new CodeDoesntExistException(Responses.CODE_NON_EXIST.getResponse());
+            return statusResponseManager.buildWithMessage(Responses.CODE_LOSE.getResponse());
         }
     }
 
-    public List<Code> getAllCodesByLotteryId(Long id) {
-        LOGGER.info("Getting all codes information by lottery id #" + id);
-        return new ArrayList<>(codeDAO.findCodesByLotteryId(id));
+
+    public String getEmailByCodeAndLotteryId(String winnerCode, Long id) throws CodeDoesntExistException {
+        return codeEntityManager.getEmailByCodeAndLotteryId(winnerCode,id);
     }
 
-    public String getEmailByCodeAndLotteryId(String code, Long id) throws CodeDoesntExistException {
-        LOGGER.info("Getting email by code" + code + " and lottery id #" + id);
+    public Code getCodeByParticipatingCodeAndLotteryId(String winnerCode, Long id) throws CodeDoesntExistException {
+        return codeEntityManager.getCodeByParticipatingCodeAndLotteryId(winnerCode,id);
+    }
 
-        Optional<Code> possibleCode = codeDAO.findCodeByParticipatingCodeAndLotteryId(code,
-                id);
-
-        if (possibleCode.isPresent()) {
-            return possibleCode.get().getOwnerEmail();
-        } else {
-            LOGGER.warn("Could not find code information for code:" + code);
-            throw new CodeDoesntExistException(Responses.CODE_NON_EXIST.getResponse());
-        }
+    public List<Code> getAllCodesByLotteryId(Long lotteryId) {
+        return codeEntityManager.getAllCodesByLotteryId(lotteryId);
     }
 }
